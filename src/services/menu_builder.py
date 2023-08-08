1e6023d022937104997bc23e1f98b96ae12b73c9
andrejvb <andre.jvb@gmail.com>
2023-08-08 17:22:56 -0300

    fourth requirement

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -27,3 +27,22 @@ class MenuBuilder:
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
         pass
+        menu = []
+        for dish in self.menu_data.dishes:
+            ingredient_availability = all(
+                ingredient in self.inventory.inventory
+                for ingredient in dish.get_ingredients()
+            )
+            if (
+                restriction not in dish.get_restrictions()
+                and ingredient_availability
+            ):
+                menu_item = {                    
+                        "dish_name": dish.name,
+                        "ingredients": dish.get_ingredients(),
+                        "price": dish.price,
+                        "restrictions": dish.get_restrictions(),
+                    }
+                
+                menu.append(menu_item)
+        return menu
